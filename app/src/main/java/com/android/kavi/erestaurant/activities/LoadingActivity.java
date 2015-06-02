63734c755a9db123353bd7868ae79df9669be60f
Kavimal Wijewardana <kavi707@gmail.com>
2015-06-02 17:04:59 +0530

    Removed action bar from LoadingActivity

--- app/src/main/java/com/android/kavi/erestaurant/activities/LoadingActivity.java
+++ app/src/main/java/com/android/kavi/erestaurant/activities/LoadingActivity.java
@@ -1,5 +1,6 @@
 package com.android.kavi.erestaurant.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 import com.android.kavi.erestaurant.R;
 
 
-public class LoadingActivity extends ActionBarActivity {
+public class LoadingActivity extends Activity {
 
     private TextView loadingTitleTextView;
 
@@ -29,32 +30,10 @@ public class LoadingActivity extends ActionBarActivity {
         loadingTitleTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent tabsIntent = new Intent(LoadingActivity.this, TabsActivity.class);
-                startActivity(tabsIntent);
+                Intent loginIntent = new Intent(LoadingActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
                 finish();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
