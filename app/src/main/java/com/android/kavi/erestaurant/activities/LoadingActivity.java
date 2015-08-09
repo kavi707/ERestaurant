aad32d5be9ef75b3bba2bb80eb3ed6bbb9eea486
Kavimal Wijewardana <kavi707@gmail.com>
2015-08-09 13:22:19 +0530

    Create splash & login views according to propose views

--- app/src/main/java/com/android/kavi/erestaurant/activities/LoadingActivity.java
+++ app/src/main/java/com/android/kavi/erestaurant/activities/LoadingActivity.java
@@ -3,6 +3,7 @@ package com.android.kavi.erestaurant.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,7 +15,8 @@ import com.android.kavi.erestaurant.R;
  */
 public class LoadingActivity extends Activity {
 
-    private TextView loadingTitleTextView;
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +27,23 @@ public class LoadingActivity extends Activity {
     }
 
     private void setUpView() {
-        loadingTitleTextView = (TextView) findViewById(R.id.loadingTitleTextView);
+        new Handler().postDelayed(new Runnable() {
 
-        loadingTitleTextView.setOnClickListener(new View.OnClickListener() {
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
             @Override
-            public void onClick(View v) {
-                Intent loginIntent = new Intent(LoadingActivity.this, LoginActivity.class);
-                startActivity(loginIntent);
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(LoadingActivity.this, LoginActivity.class);
+                startActivity(i);
+
+                // close this activity
                 finish();
             }
-        });
+        }, SPLASH_TIME_OUT);
     }
 }
