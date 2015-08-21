080b645f51dede0562cbfc0fa3283e350e5cde81
Kavimal Wijewardana <kavi707@gmail.com>
2015-08-21 08:39:02 +0530

    Create single item view

---
+++ app/src/main/java/com/android/kavi/erestaurant/activities/SingleItemActivity.java
@@ -0,0 +1,20 @@
+package com.android.kavi.erestaurant.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.kavi.erestaurant.R;
+
+/**
+ * Created by kavi707 on 8/21/15.
+ *
+ * @author Kavimal Wijewardana <kavi707@gmail.com>
+ */
+public class SingleItemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_item);
+    }
+}
