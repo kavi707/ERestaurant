0ed22b064aca96dffc06db4b133bbd215a694c55
Kavimal Wijewardana <kavi707@gmail.com>
2015-09-06 12:39:38 +0530

    Did new UI changes in the application

--- app/src/main/java/com/android/kavi/erestaurant/activities/MainTabHostActivity.java
+++ app/src/main/java/com/android/kavi/erestaurant/activities/MainTabHostActivity.java
@@ -34,28 +34,28 @@ public class MainTabHostActivity extends TabActivity {
         Intent intentHome = new Intent().setClass(this, HomeActivity.class);
         TabHost.TabSpec tabSpecHome = tabHost
                 .newTabSpec("Home")
-                .setIndicator("", ressources.getDrawable(R.drawable.home_image))
+                .setIndicator("", ressources.getDrawable(R.drawable.footer_home))
                 .setContent(intentHome);
 
         // Current Order tab
         Intent intentCurrentOrder = new Intent().setClass(this, CurrentOrderActivity.class);
         TabHost.TabSpec tabSpecCurrentOrder = tabHost
                 .newTabSpec("Current Order")
-                .setIndicator("", ressources.getDrawable(R.drawable.search_image))
+                .setIndicator("", ressources.getDrawable(R.drawable.footer_current_order))
                 .setContent(intentCurrentOrder);
 
         // Active Tables tab
         Intent intentActiveTabs = new Intent().setClass(this, ActiveTablesActivity.class);
         TabHost.TabSpec tabSpecActiveTables = tabHost
                 .newTabSpec("Active Tables")
-                .setIndicator("", ressources.getDrawable(R.drawable.table_image))
+                .setIndicator("", ressources.getDrawable(R.drawable.footer_active_tables))
                 .setContent(intentActiveTabs);
 
         // Ready Items tab
         Intent intentReadyItems = new Intent().setClass(this, ReadyItemsActivity.class);
         TabHost.TabSpec tabSpecReadyItems = tabHost
                 .newTabSpec("Ready Items")
-                .setIndicator("", ressources.getDrawable(R.drawable.thubms_image))
+                .setIndicator("", ressources.getDrawable(R.drawable.footer_ready_items))
                 .setContent(intentReadyItems);
 
 
--- app/src/main/java/com/android/kavi/erestaurant/services/connections/ApiCalls.java
+++ app/src/main/java/com/android/kavi/erestaurant/services/connections/ApiCalls.java
@@ -1,9 +1,17 @@
 package com.android.kavi.erestaurant.services.connections;
 
+import android.content.Context;
+
 /**
  * Created by kavi707 on 6/23/15.
  *
  * @author Kavimal Wijewardana <kavi707@gmail.com>
  */
-public class ApiCalls extends AsyncApiConnector {
+public class ApiCalls {
+
+    private Context context;
+
+    public ApiCalls(Context context) {
+        this.context = context;
+    }
 }
